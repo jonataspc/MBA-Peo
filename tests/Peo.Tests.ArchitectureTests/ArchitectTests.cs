be4694ceb46557f6e119802c370250560b54c54d
Jonatas Cruz <jonataspc@gmail.com>
2025-04-24 22:15:02 -0300

    tests: adding architect tests

---
+++ tests/Peo.Tests.ArchitectureTests/ArchitectTests.cs
@@ -0,0 +1,108 @@
+using FluentAssertions;
+using NetArchTest.Rules;
+using Peo.Core.Interfaces.Data;
+using Peo.Tests.ArchitectureTests.Extensions;
+using System.Reflection;
+
+namespace Peo.Tests.ArchitectureTests;
+
+public class ArchitectTests
+{
+    private static IEnumerable<Assembly> GetDomainAssemblies()
+    {
+        return [
+            typeof(Peo.Identity.Domain.Interfaces.Data.IUserRepository ).Assembly,
+            typeof(Peo.Billing.Domain.Dtos.CreditCard).Assembly,
+            typeof(Peo.ContentManagement.Domain.Entities.Course).Assembly,
+            typeof(Peo.StudentManagement.Domain.Entities.Certificate).Assembly,
+            typeof(Peo.Core.DomainObjects.AccessRoles).Assembly,
+            ];
+    }
+
+    private static IEnumerable<Assembly> GetAllAssemblies()
+    {
+        var list = new List<Assembly>
+        {
+            typeof(Peo.Web.Api.Services.AppIdentityUser).Assembly,
+            typeof(Peo.Core.Web.Api.IEndpoint).Assembly,
+            typeof(Peo.Core.Infra.Data.Contexts.Base.DbContextBase ).Assembly,
+            typeof(Peo.StudentManagement.Infra.Data.Helpers.StudentManagementeDbMigrationHelpers).Assembly,
+            typeof(Peo.StudentManagement.Application.Endpoints.StudentEndpoints).Assembly,
+            typeof(Peo.Identity.Infra.Data.Helpers.IdentityDbMigrationHelpers).Assembly,
+            typeof(Peo.Identity.Application.Services.TokenService).Assembly,
+            typeof(Peo.ContentManagement.Infra.Data.Helpers.ContentManagementeDbMigrationHelpers).Assembly,
+            typeof(Peo.ContentManagement.Application.Services.CourseLessonService).Assembly,
+            typeof(Peo.Billing.Integrations.Paypal.Services .PaypalBrokerService ).Assembly,
+            typeof(Peo.Billing.Infra.Data.Helpers.BillingDbMigrationHelpers).Assembly,
+            typeof(Peo.Billing.Application.Services.PaymentService ).Assembly,
+        };
+
+        list.AddRange(GetDomainAssemblies());
+
+        return list;
+    }
+
+    [Fact]
+    public void Domain_Must_Not_Reference_External_Libraries()
+    {
+        var result = Types
+            .InAssemblies(GetDomainAssemblies())
+            .Should()
+            .OnlyHaveDependencyOn(
+            "System",
+            "Microsoft",
+            "Peo.Identity.Domain",
+            "Peo.Core",
+            "Peo.Billing.Domain",
+            "Peo.ContentManagement.Domain",
+            "Peo.StudentManagement.Domain")
+            .GetResult();
+
+        result.IsSuccessful.Should().BeTrue(result.GetDetails());
+    }
+
+    [Fact]
+    public void DataAccess_Must_Reside_In_Infra()
+    {
+        var result = Types.InAssemblies(GetAllAssemblies())
+          .That()
+          .HaveDependencyOnAll("Microsoft.EntityFrameworkCore")
+          .And()
+          .ResideInNamespace("Peo")
+          .Should()
+          .ResideInNamespaceMatching(@"^Peo.*Infra\.Data.*")
+          .GetResult();
+
+        result.IsSuccessful.Should().BeTrue(result.GetDetails());
+    }
+
+    [Fact]
+    public void Repositories_Must_Have_Name_Ending_In_Repository()
+    {
+        var result = Types.InAssemblies(GetAllAssemblies())
+          .That()
+          .AreClasses()
+          .And()
+          .ImplementInterface(typeof(IRepository<>))
+          .Should()
+          .HaveNameEndingWith("Repository")
+          .And()
+          .ResideInNamespaceMatching(@"^Peo.*Infra\.Data.*")
+          .GetResult();
+
+        result.IsSuccessful.Should().BeTrue(result.GetDetails());
+    }
+
+    [Fact]
+    public void Interfaces_Must_Start_Whith_I()
+    {
+        var result = Types.InAssemblies(GetAllAssemblies())
+        .That()
+        .AreInterfaces()
+        .Should()
+        .HaveNameStartingWith("I")
+        .GetResult();
+
+        result.IsSuccessful.Should().BeTrue(result.GetDetails());
+    }
+}
