694f6741b6fd1809d10be01e16f3e76f2f4b151b
Jonatas Cruz <jonataspc@gmail.com>
2025-05-19 18:45:47 -0300

    doc: atualização FEEDBACK

--- src/Peo.Web.Api/MIddlewares/GlobalExceptionHandler.cs
+++ src/Peo.Web.Api/MIddlewares/GlobalExceptionHandler.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Peo.Web.Api.MIddlewares
+namespace Peo.Web.Api.Middlewares
 {
     internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
     {
